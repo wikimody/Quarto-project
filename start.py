0eabe3510164992bee37538c3274a6bb848d7f14
How2tutorial <danielbog88@gmail.com>
2023-12-06 22:47:17 +0100

    Poprawki wskazane przy pull requescie (klasa Player traci dostęp do planszy [Board] a logika stawiania figura została przekazana klasie QuartoGame, wskazywanie figur po wartości zamiast kolejności, kilka zmian w nazewnictwie metod klas Piece i Board, od teraz Game to QuartoGame, nowa funkcja find_piece(piece_value) klasy QuartoGame)

--- start.py
+++ start.py
@@ -3,36 +3,6 @@ import random
 class Player:
     def __init__(self, name):
         self._name = name
-        self._board = None
-
-    def join_board(self, board):
-        self._board = board
-
-    def place(self, piece):
-        print("Otrzymana figura: \n%s" % piece)
-        while True:
-            row = input("Wybierz rząd (1-4): ")
-            while not (row.isnumeric() and 1 <= int(row) <= 4):
-                row = input("Wybrany rząd nie istnieje, ponów wybór: ")
-            row = int(row)
-            
-            col = input("Wybierz kolumnę (1-4): ")
-            while not (col.isnumeric() and 1 <= int(col) <= 4):
-                col = input("Wybrana kolumna nie istnieje, ponów wybór: ")
-            col = int(col)
-            
-            if self._board.is_avaliable(row, col):
-                self._board.place(piece, row, col)
-                quarto_call = input("Czy chcesz zawołać quatro? Wybierz TAK/NIE [t/n]: ")
-                while not quarto_call.upper() in ("T", "TAK", "N", "NIE"):
-                    quarto_call = input("Odpowiedź musi być w formacie TAK lub NIE [t/n]: ")
-                if quarto_call.upper() in ("T", "TAK"):
-                    if self._board.is_winning(piece):
-                        return True
-                    print("Podana figura nie tworzy Quatro")
-                return False
-            
-            print("Wybrane pole jest zajęte, wybierz inne.")
 
     def __str__(self):
         return self._name
@@ -58,7 +28,7 @@ class Piece:
             piece_value -= 2
         self._bin_value[3] = piece_value # height
 
-    def is_empty(self):
+    def is_idle(self):
         return self._bin_value == None
 
     def is_white(self):
@@ -67,7 +37,7 @@ class Piece:
     def is_circle(self):
         return self._bin_value[1] == 1
 
-    def is_square(self):
+    def is_filled(self):
         return self._bin_value[2] == 1
 
     def is_high(self):
@@ -80,7 +50,7 @@ class Piece:
         return self._bin_value
 
     def __str__(self):
-        return "%d%d\n%d%d" % (self._bin_value[0], self._bin_value[1], self._bin_value[2], self._bin_value[3])
+        return f"{self._bin_value[0]}{self._bin_value[1]}\n{self._bin_value[2]}{self._bin_value[3]}"
 
 class Board:
     def __init__(self): # creates board filled with empty pieces
@@ -89,17 +59,19 @@ class Board:
     def has_empty_tiles(self):
         for row in self._board:
             for tile in row:
-                if tile.is_empty():
+                if tile.is_idle():
                     return True
         return False
 
+    # user inputs row and col values in [1-4] format
     def is_avaliable(self, row, col):
-        return self._board[row - 1][col - 1].is_empty()
+        return self._board[row - 1][col - 1].is_idle()
 
     def place(self, piece, row, col):
         self._board[row - 1][col - 1] = piece
 
-    def is_winning(self, piece): # Temporarly false
+    # returns True if piece creates a Quarto, False otherwise
+    def is_validating(self, piece): # Temporarly false
         return False
 
     def format(self): # returns board in string format "t t ... t t" (t - 16 tile values)
@@ -113,7 +85,7 @@ class Board:
             readable_row = "%d|" % (m+1)
             for n in range(4):
                 piece = self._board[m][n]
-                if piece.is_empty():
+                if piece.is_idle():
                     readable_row += "    |"
                 else:
                     piece_binary = piece.binary()
@@ -122,7 +94,7 @@ class Board:
             readable_row += "%d|" % (m+1)
             for n in range(4):
                 piece = self._board[m][n]
-                if piece.is_empty():
+                if piece.is_idle():
                     readable_row += "    |"
                 else:
                     piece_binary = piece.binary()
@@ -133,14 +105,12 @@ class Board:
 
         return readable_board
 
-class Game:
+class QuartoGame:
     def __init__(self, player1, player2):
         self._players = [player1, player2]
         self._turn = random.randint(0, 1)
         self._avaliable_pieces = [Piece(val) for val in range(16)]
         self._board = Board()
-        player1.join_board(self._board)
-        player2.join_board(self._board)
 
     def next_turn(self):
         self._turn = (self._turn + 1) % 2
@@ -151,20 +121,25 @@ class Game:
     def other_player(self):
         return self._players[(self._turn + 1) % 2]
 
-    def remove_piece(self, piece_ord):
-        self._avaliable_pieces = self._avaliable_pieces[:piece_ord]
+    # looks for a piece with given value and returns it (returns None if not found)
+    def find_piece(self, piece_value):
+        for piece in self._avaliable_pieces:
+            if piece.decimal() == piece_value:
+                return piece
+        return None
 
-    def format_avaliable_pieces(self): # returns list of pieces in readable format (string)
-        number_of_pieces = len(self._avaliable_pieces)
-
+    # returns list of pieces in readable format (string)
+    def format_avaliable_pieces(self):
         readable_pieces = "|"
-        for i in range(1, number_of_pieces+1): # column spacing
-            if i < 10:
-                readable_pieces += " %d|" % i
+        for piece in self._avaliable_pieces:
+            piece_value = piece.decimal()
+            # column spacing adjusting
+            if piece_value < 10:
+                readable_pieces += " %d|" % piece_value
             else:
-                readable_pieces += "%d|"  % i
+                readable_pieces += "%d|"  % piece_value
         readable_pieces += "\n"
-        readable_pieces += "|--" * number_of_pieces + "|\n"
+        readable_pieces += "|--" * len(self._avaliable_pieces) + "|\n"
 
         readable_pieces += "|"
         for piece in self._avaliable_pieces:
@@ -188,32 +163,54 @@ class Game:
         print("Gracz %s stawia figurę." % self.other_player())
 
     def start(self):
-        while self._board.has_empty_tiles():
-            current_player = self.current_player()
-            other_player = self.other_player()
+        # Game is going on while there are pieces to choose from
+        while len(self._avaliable_pieces):
+            current_player = self.current_player() # player that picks a piece
+            other_player = self.other_player() # player that places it down
             
             self.display_preturn_state()
-            
-            piece_ord = input("Wybierz figurę dla przeciwnika: ")
-            while not (piece_ord.isnumeric() and 1 <= int(piece_ord) <= len(self._avaliable_pieces)):
-                piece_ord = input("Podana figura nie istnieje, wybierz inną: ")
-            piece_ord = int(piece_ord) - 1
 
-            chosen_piece = self._avaliable_pieces[piece_ord]
-            del self._avaliable_pieces[piece_ord]
+            # current player chooses piece for the opponent
+            piece_value = input("Wybierz figurę dla przeciwnika: ")
+            piece = self.find_piece(int(piece_value)) if piece_value.isnumeric() else None
+            while piece == None:
+                piece_value = input("Figura o podanej wartości nie istnieje, wybierz inną: ")
+                piece = self.find_piece(int(piece_value)) if piece_value.isnumeric() else None
+            self._avaliable_pieces.remove(piece)
             
             self.clear_view()
             self.display_postturn_state()
+
+            print("Otrzymana figura: \n%s" % piece)
+            while True: # loops until piece is correctly placed on board
+                row = input("Wybierz rząd (1-4): ")
+                while not (row.isnumeric() and 1 <= int(row) <= 4):
+                    row = input("Wybrany rząd nie istnieje, ponów wybór: ")
+                row = int(row)
+                
+                col = input("Wybierz kolumnę (1-4): ")
+                while not (col.isnumeric() and 1 <= int(col) <= 4):
+                    col = input("Wybrana kolumna nie istnieje, ponów wybór: ")
+                col = int(col)
+                
+                if self._board.is_avaliable(row, col): # True if piece can be correctly placed
+                    self._board.place(piece, row, col)
+                    quarto_call = input("Czy chcesz zawołać Quarto? Wybierz TAK/NIE [t/n]: ")
+                    while not quarto_call.upper() in ("T", "TAK", "N", "NIE"):
+                        quarto_call = input("Odpowiedź musi być w formacie TAK lub NIE [t/n]: ")
+                    if quarto_call.upper() in ("T", "TAK"):
+                        if self._board.is_validating(piece):
+                            return other_player # players successfuly calls Quarto and wins the game
+                        input("Podana figura nie tworzy Quarto, wciśnij Enter, aby kontynuować...")
+                    break # piece is placed - breaks the loop
             
-            quarto_call = other_player.place(chosen_piece)
-            if quarto_call:
-                return other_player
+                print("Wybrane pole jest zajęte, wybierz inne.")
 
             self.clear_view()
             self.next_turn()
 
         self.clear_view()
-        return None
+        return None # game ends without anyoune winning
     
     def clear_view(self):
         print("\n" * 28)
@@ -222,7 +219,7 @@ class Game:
 if __name__ == '__main__':
     # player1 = Player(input("Wprowadź nazwę pierwszego gracza: "))
     # player2 = Player(input("Wprowadź nazwę drugiego gracza: "))
-    game = Game(Player("A"), Player("B"))
+    game = QuartoGame(Player("A"), Player("B"))
     winner = game.start()
     if (winner != None):
         print("Zwyciężył gracz %s" % winner)
