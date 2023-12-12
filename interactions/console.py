91f0a6cdc34013d7396129d2485a87fa20227c2d
wikimody <58008547+wikimody@users.noreply.github.com>
2023-12-12 13:25:19 +0100

    Update console.py

--- interactions/console.py
+++ interactions/console.py
@@ -1,42 +1,52 @@
+import os
 class Console:
+
     def clear_view(): # czyszczenie ekranu TYLKO NA LINUXS!!!
-        import os
         os.system('clear')
 
     def output (text): # Print w funkcji
         print(text)
 
-    def input_text (wejscie): # Zamiana na string i return
-        text = str(wejscie)
+    def input_text (entry): # Zamiana na string i return
+        text = str(entry)
         return text
 
     def input_number (stri,rang): # czy jest to liczba w  liście
         try:
             numer = int(stri)
-            czy_w_range = False
+            is_in_range = False
             for x in rang:
               if x == numer:
-                    czy_w_range = True
-            if czy_w_range == True:
+                    is_in_range = True
+            if is_in_range == True:
                 return numer
             else:
-                return False
+                a = ['Podaj całkowity numer należący do listy: ', str(rang)]
+                a = ''.join(a)
+                newn = input(a)
+                Console.input_number(newn, rang)
         except ValueError:
-            return False
+            a = ['Podaj całkowity numer należący do listy: ',str(rang) ]
+            a = ''.join(a)
+            newn = input(a)
+            Console.input_number(newn, rang)
 
     def input_numer_in_range (stri,beginr,endr): #czy jest to liczba pomiędzy tymi
         try:
             numer = int(stri)
-            czy_w_range = False
+            is_in_range = False
             for x in range(beginr, endr + 1):  # pomiędzy łacznie z liczbą końcową
                 if x == numer:
-                    czy_w_range = True
-            if czy_w_range == True:
+                    is_in_range = True
+            if is_in_range == True:
                 return numer
             else:
-                return False
+                a = ['Podaj całkowity numer w zakresie: ', str(beginr), ' do ', str(endr)]
+                a = ''.join(a)
+                newn = input(a)
+                return Console.input_numer_in_range(newn,beginr,endr)
         except ValueError:
-            return False
-
-
-
+            a = ['Podaj całkowity numer w zakresie: ',str(beginr),  ' do ',str(endr)]
+            a = ''.join(a)
+            newn = input(a)
+            return Console.input_numer_in_range(newn, beginr, endr)
