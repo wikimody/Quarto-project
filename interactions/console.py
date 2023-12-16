51f14c87ff14d35a381c33b4acd826bb4bea609d
wikimody <58008547+wikimody@users.noreply.github.com>
2023-12-16 11:23:07 +0100

    Finalizing console.py
    Upiększanie kodu, optymalizacja i ostatnie zmiany

--- interactions/console.py
+++ interactions/console.py
@@ -3,70 +3,55 @@ import os
 class Console:
 
     @staticmethod
-    def is_int(stri):  # czy można zmienić w int
+    def is_int(string_to_check):  # Sprawdza czy dany string można zmienić w int, True na tak False na nie
         try:
-            a = int(stri)
+            a = int(string_to_check)
             return True
         except ValueError:
             return False
 
     @staticmethod
-    def clear_view():  # czyszczenie ekranu TYLKO NA LINUXS!!!
+    def clear_view():  # Czyszczenie ekranu DZIAŁA TYLKO NA LINUXS!!!
         os.system('clear')
 
     @staticmethod
-    def output(text):  # Print w funkcji
+    def output(text):  # Pisze wysłany tekst
         print(text)
 
     @staticmethod
-    def input_text(entry):  # Wzięcie string i return
-        text = input(str(entry))
-        return text
+    def input_text(question):  # Zadaje pytanie i zwraca odpowiedź
+        entry = input(str(question))
+        return entry
 
     @staticmethod
-    def input_number(stri, rang):  # Czy jest to liczba w  liście
-        entry = input(stri)
+    def input_number(question, posible_numbers):  # Zadaje pytanie i zwraca liczbę z przesłanej listy
+        entry = input(question)
         while True:
             if Console.is_int(entry):
-                numer = int(entry)
-                is_in_range = False
-                for x in rang:
-                    if x == numer:
-                        is_in_range = True
-                if is_in_range:
-                    return numer
+                number = int(entry)
+                if number in posible_numbers:
+                    return number
                 else:
-                    a = ['Ten numer nie nalerzy do listy. Podaj numer należący do listy: ', str(rang)]
-                    a = ''.join(a)
-                    entry = input(a)
-
+                    entry = input(f'Ten numer nie nalerzy do listy. Podaj numer należący do listy: {str(posible_numbers)}')
             else:
-                a = ['To nie jest numer. Podaj numer należący do listy: ', str(rang)]
-                a = ''.join(a)
-                entry = input(a)
+                entry = input(f'To nie jest numer. Podaj numer należący do listy: {str(posible_numbers)}')
 
     @staticmethod
-    def input_numer_in_range(stri, beginr, endr):  # czy jest to liczba pomiędzy tymi
-        entry = input(stri)
+    def input_numer_in_range(question, beginr, endr):  # Zadaje pytanie i oddaje liczbę w danym zakresie
+        entry = input(question)
         while True:
             if Console.is_int(entry):
                 numer = int(entry)
                 if beginr <= numer <= endr:
                     return numer
                 else:
-                    a = ['Ten numer nie nalerzy do zakresu. Podaj całkowity numer w zakresie: ', str(beginr), ' do ',
-                         str(endr)]
-                    a = ''.join(a)
-                    entry = input(a)
+                    entry = input(f'Ten numer nie nalerzy do zakresu. Podaj całkowity numer w zakresie: {str(beginr)} do {str(endr)}')
             else:
-                a = ['To nie jest numer. Podaj całkowity numer w zakresie: ', str(beginr), ' do ', str(endr)]
-                a = ''.join(a)
-                entry = input(a)
+                entry = input(f'To nie jest numer. Podaj całkowity numer w zakresie: {str(beginr)} do {str(endr)}')
 
     @staticmethod
-    def input_yes_or_no(stri):  # tak czy nie
-        a = stri + ', odpowiedz T na tak lub N na nie'
-        entry = input(a)
+    def input_yes_or_no(question):  # Zadaje pytanie i zwraca True dla tak i False dle nie
+        entry = input(f'{question}, odpowiedz T na tak lub N na nie')
         while True:
             if entry == 't' or entry == 'T':
                 return True
