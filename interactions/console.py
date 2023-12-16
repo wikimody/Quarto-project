cca03bee0cad0fda97c13cf4c6683c91bf700af5
wikimody <58008547+wikimody@users.noreply.github.com>
2023-12-16 16:26:08 +0100

    Poprawka finalnego komitu console.py

--- interactions/console.py
+++ interactions/console.py
@@ -5,7 +5,7 @@ class Console:
     @staticmethod
     def is_int(string_to_check):  # Sprawdza czy dany string można zmienić w int, True na tak False na nie
         try:
-            a = int(string_to_check)
+            int(string_to_check)
             return True
         except ValueError:
             return False
@@ -32,29 +32,29 @@ class Console:
                 if number in posible_numbers:
                     return number
                 else:
-                    entry = input(f'Ten numer nie nalerzy do listy. Podaj numer należący do listy: {str(posible_numbers)}')
+                    entry = input(f'Ten numer nie nalerzy do listy. Podaj numer należący do listy: {posible_numbers} ')
             else:
-                entry = input(f'To nie jest numer. Podaj numer należący do listy: {str(posible_numbers)}')
+                entry = input(f'To nie jest numer. Podaj numer należący do listy: {posible_numbers} ')
 
     @staticmethod
-    def input_numer_in_range(question, beginr, endr):  # Zadaje pytanie i oddaje liczbę w danym zakresie
+    def input_numer_in_range(question, begining_of_range, end_of_range):  # Zadaje pytanie i oddaje liczbę w danym zakresie
         entry = input(question)
         while True:
             if Console.is_int(entry):
-                numer = int(entry)
-                if beginr <= numer <= endr:
-                    return numer
+                number = int(entry)
+                if begining_of_range <= number <= end_of_range:
+                    return number
                 else:
-                    entry = input(f'Ten numer nie nalerzy do zakresu. Podaj całkowity numer w zakresie: {str(beginr)} do {str(endr)}')
+                    entry = input(f'Ten numer nie nalerzy do zakresu. Podaj całkowity numer w zakresie: {begining_of_range} do {end_of_range} ')
             else:
-                entry = input(f'To nie jest numer. Podaj całkowity numer w zakresie: {str(beginr)} do {str(endr)}')
+                entry = input(f'To nie jest numer. Podaj całkowity numer w zakresie: {begining_of_range} do {end_of_range} ')
 
     @staticmethod
     def input_yes_or_no(question):  # Zadaje pytanie i zwraca True dla tak i False dle nie
-        entry = input(f'{question}, odpowiedz T na tak lub N na nie')
+        entry = input(f'{question}, odpowiedz T na tak lub N na nie ')
         while True:
             if entry == 't' or entry == 'T':
                 return True
             if entry == 'n' or entry == 'N':
                 return False
-            entry = input('T na tak lub N na nie')
+            entry = input('T na tak lub N na nie ')
