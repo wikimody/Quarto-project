37e01625f7ca21abb0cdc617512bdeae6df6813d
Keitharnemen <84507498+Keitharnemen@users.noreply.github.com>
2024-01-16 15:50:53 +0100

    Zmiana technologii uruchamiania programow .exe w cpp_adapter

--- cpp_bots/cpp_adapter.py
+++ cpp_bots/cpp_adapter.py
@@ -1,16 +1,11 @@
-import os
+import subprocess
+import platform
 
+def execute_cpp(program_path : str, program_args : str) -> str:
+    run_args = [program_path] + program_args.split(sep = " ")
 
-class CppAdapter:
-    @staticmethod
-    def compile_cpp(cpp_name, executable_name):
-        compile_command = f"g++ -o {executable_name} {cpp_name}"
-        compilation_result = os.system(compile_command)
-        if compilation_result != 0:
-            print(f"Compilation failed with error code {compilation_result}")
-            exit()
-
-    @staticmethod
-    def execute_cpp(executable_program, executable_input):
-        execution_result = os.popen(f"./{executable_program} {executable_input}").read()
-        return execution_result
+    if platform.system() == 'Linux':
+        run_args = ['wine'] + run_args
+    
+    result = subprocess.run(run_args, capture_output=True)
+    return result.stdout
