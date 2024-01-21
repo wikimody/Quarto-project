aa54b6f6b9a2a7a5421cc410a664c9b69a6e5e13
Dancer77 <150295806+Dancer77@users.noreply.github.com>
2024-01-21 15:14:44 +0100

    Update 1_bot.cpp
    Podajemy współrzędne od 1 do 4 (wcześniej było od 0 do 3)

--- 1_bot.cpp
+++ 1_bot.cpp
@@ -51,6 +51,6 @@ int main(int argc, char *argv[])
         }
     }
 
-    cout << wiersz << kolumna << pionki[pionek] << "\n";
+    cout << wiersz+1 << kolumna+1 << pionki[pionek] << "\n";
     return 0;
 }
