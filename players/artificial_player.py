64d83e435210cb6ab2d2abb46995fa6fd493f51f
Keitharnemen <84507498+Keitharnemen@users.noreply.github.com>
2024-01-22 00:52:51 +0100

    Zmiana w klasie ArtificialPlayer, aby boty mogły odpowiednio przetwarzać dane.

--- players/artificial_player.py
+++ players/artificial_player.py
@@ -1,27 +1,27 @@
 from .player import Player
+from quarto_game.piece import Piece
+import cpp_bots.cpp_adapter as cppA
+import time
+from quarto_game.board import Board
 
 
-# Zaimplementowałem od razu najgłupszego możliwego bota
 class ArtificialPlayer(Player):
-    def __init__(self, name, path):
-        self._name = name
-        self._path = path
+    def __init__(self, level : int, board : Board) -> None:
+        self._level = level
+        self._board = board
+        self._piece_number = -1
 
-    def choose_piece(self, pieces_values):  # pieces - lista wartości pionów (decymalna)
-        # W tym miejscu bot wybiera, którą figurę przekazać drugiemu graczowi
-        # Podanie niepoprawnej wartości może poskutkować błędem programu
-        # oczekiwana wartość: jedna z wartości listy pieces
-        return pieces_values[0]
+    def choose_piece(self, pieces : list(int)) -> int: 
+        if self._piece_number == -1:
+            return time.time() % 16 # XD - ale jest to jakaś losowość
+        return self._piece_number 
 
-    def where_place_piece(self, piece=None, board=None):  # piece - obiekt klasy Piece
-        # W tym miejscu bot wybiera, gdzie postawić figurę
-        # Podanie niepoprawnej wartości poskutkuje ponownym wywołaniem funkcji (możliwa nieskończona pętla)
-        # oczekiwana wartość: krotka (a, b) [1 <= a,b <= 4]
+    def place_piece(self, piece : Piece) -> list(int, int): 
+        args = cppA.generate_args((self._board), piece.decimal())
+        result = cppA.execute_cpp(f"../cpp_bots/bin/bot{self._level}",  args)
+        result_list = cppA.receive_args(result)
+        self._piece_number = result_list[2]
+        return(result_list[0], result_list[1])
 
-        for row in range(1, 5):
-            for col in range(1, 5):
-                if board.is_avaliable(row, col):
-                    return row, col
-
-    def __str__(self):
-        return self._name
+    def __str__(self) -> str:
+        return "Bot (poziom %d)" % self._level
