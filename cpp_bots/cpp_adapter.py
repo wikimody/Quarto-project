6bff9ace2a0073e653f30c7513efb6eacda0f730
Keitharnemen <84507498+Keitharnemen@users.noreply.github.com>
2024-01-21 15:49:38 +0100

    Dodanie funkcji generującej odpowiednią postać dla argumentów dla botów

--- cpp_bots/cpp_adapter.py
+++ cpp_bots/cpp_adapter.py
@@ -1,11 +1,22 @@
 import subprocess
 import platform
-
+from quarto_game.board import Board
 def execute_cpp(program_path : str, program_args : str) -> str:
     run_args = [program_path] + program_args.split(sep = " ")
 
     if platform.system() == 'Linux':
         run_args = ['wine'] + run_args
     
-    result = subprocess.run(run_args, capture_output=True)
+    result = subprocess.run(run_args, capture_output=True, encoding='utf-8')
     return result.stdout
+
+def generate_args(board : Board, piece_choice : int) -> str:
+    result = ""
+    for i in range(4):
+        for j in range(4):
+            if board[i][j].decimal() == -1: # place is empty
+                result += 'p'
+            else:
+                result += hex(board[i][j].decimal)[2::]
+    result += hex(piece_choice)[2::]
+    return result
